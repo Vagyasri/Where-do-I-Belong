d4b12a5d7c287be229294f19535dd0b204ab0716
Vagyasri <vagyasri2011@gmail.com>
2021-11-15 10:06:10 +0100

    Add test cases

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ function getIndexToIns(arr, num) {
   if (arr.length === 0) {
     return 0;
   }
-  for (let i = 0; i < arr.length; i++) {
+  for (let i = 0; i < arr.length; i += 1) {
     arr.sort((a, b) => a - b);
     if (num <= arr[i]) {
       return arr.indexOf(arr[i]);
@@ -14,3 +14,12 @@ function getIndexToIns(arr, num) {
     }
   }
 }
+
+console.log(getIndexToIns([10, 20, 30, 40, 50], 35)); // 3
+console.log(getIndexToIns([10, 20, 30, 40, 50], 30)); // 2
+console.log(getIndexToIns([40, 60], 50)); // 1
+console.log(getIndexToIns([3, 10, 5], 3)); // 0
+console.log(getIndexToIns([5, 3, 20, 3], 5)); // 2
+console.log(getIndexToIns([2, 20, 10], 19)); // 2
+console.log(getIndexToIns([2, 5, 10], 15)); // 3
+console.log(getIndexToIns([], 1)); // 0
