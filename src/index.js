aeca97f93ba05762f3417cbb4b8918bc3fe37315
Vagyasri <vagyasri2011@gmail.com>
2021-11-15 10:00:33 +0100

    Add function

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
 import './style.css';
+
+function getIndexToIns(arr, num) {
+  if (arr.length === 0) {
+    return 0;
+  }
+  for (let i = 0; i < arr.length; i++) {
+    arr.sort((a, b) => a - b);
+    if (num <= arr[i]) {
+      return arr.indexOf(arr[i]);
+    }
+    if (num > arr[arr.length - 1]) {
+      return arr.length;
+    }
+  }
+}
